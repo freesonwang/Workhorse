7b00752f11086e5728f9597c9618bcbd052ec81b
Freeson Wang <freesonwang@gmail.com>
2015-05-05 12:34:48 -0700

    Removed any dependency of workhorse.zsh files on the Workhorse library. Using switch/case

--- entry.zsh
+++ entry.zsh
@@ -1,10 +1,10 @@
 #!/bin/zsh
 
-case $1 in
-("hello")
-	echo "Hello, world!"
-	;;
-esac
+for lib_file ($this_dir/library/*) {
+	source $lib_file
+}
+
+source $WORKHORSE_TASK_SCRIPT_PATH
 
 # Workhorse - a way to quickly work on something
 # (c) Copyright Freeson Wang <freeson.wang@yahoo.com>
